13c700e9c33d97e60157654f9bbe21c94064bb8b
sara rasoulian <sara.rasoulian.dev@gmail.com>
2024-08-17 16:31:50 +0330

    🎥 Update camera frustum

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const width = window.innerWidth;
 const height = window.innerHeight;
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(40, width / height, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(45, width / height, 0.1, 1000);
 camera.position.z = 5;
 
 const renderer = new THREE.WebGLRenderer({ antialias: true });
