78bd71c4a40b930ca7350522449d35ee00227964
sara rasoulian <sara.rasoulian.dev@gmail.com>
2024-08-17 11:31:43 +0330

    🔦 Adjust light and add city lights texture

--- main.js
+++ main.js
@@ -7,42 +7,55 @@ const height = window.innerHeight;
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(50, width / height, 0.1, 1000);
-
 camera.position.z = 5;
 
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(width, height);
+
 document.body.appendChild(renderer.domElement);
+
+THREE.ColorManagement.enabled = true;
+renderer.toneMapping = THREE.ACESFilmicToneMapping;
+renderer.outputColorSpace = THREE.LinearSRGBColorSpace;
 
 const earthGroup = new THREE.Group();
 earthGroup.rotation.z = (-23.4 * Math.PI) / 180;
 scene.add(earthGroup);
 
-const stars = getStarfield({numStars: 3000});
-scene.add(stars);
-
 new OrbitControls(camera, renderer.domElement);
-const loader = new THREE.TextureLoader();
 
-const geometry = new THREE.IcosahedronGeometry(1, 20);
-const material = new THREE.MeshBasicMaterial({
-  map: loader.load("./textures/earthmap.jpg"),
+const loader = new THREE.TextureLoader();
+const geometry = new THREE.IcosahedronGeometry(1, 14);
+const material = new THREE.MeshPhongMaterial({
+  map: loader.load("./textures/earthmap1k.jpg"),
 });
 const earthMesh = new THREE.Mesh(geometry, material);
 earthGroup.add(earthMesh);
 
-const hemiLight = new THREE.HemisphereLight(0xffffff, 0x444444);
-scene.add(hemiLight);
+const lightsMat = new THREE.MeshBasicMaterial({
+  map: loader.load("./textures/earthlights1k.jpg"),
+  blending: THREE.AdditiveBlending,
+});
+const lightsMesh = new THREE.Mesh(geometry, lightsMat);
+earthGroup.add(lightsMesh);
 
-animate();
+const stars = getStarfield({ numStars: 2000 });
+scene.add(stars);
+
+const sunLight = new THREE.DirectionalLight(0xffffff, 2.0);
+sunLight.position.set(-2, 0.5, 1.5);
+scene.add(sunLight);
 
 function animate() {
   requestAnimationFrame(animate);
 
   earthMesh.rotation.y += 0.002;
-
+  lightsMesh.rotation.y += 0.002;
+  stars.rotation.y -= 0.0002;
   renderer.render(scene, camera);
 }
+
+animate();
 
 function handleWindowResize() {
   camera.aspect = window.innerWidth / window.innerHeight;
